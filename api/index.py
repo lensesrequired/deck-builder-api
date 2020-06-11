7e9cb7a1b424fe5d809d564a4ed1d8c71cfcbf02
Anna <admin@lensesrequired.com>
2020-06-10 20:45:33 -0500

    Serve a photo

--- api/index.py
+++ api/index.py
@@ -1,7 +1,37 @@
-from flask import Flask, Response
+import os
+from flask import Flask, jsonify, send_file
+import time
+from PIL import Image
+import io
+import sys
+import traceback
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+# @app.route('/', defaults={'path': ''})
+@app.route('/time')
+def time_route():
+    print('time')
+    return jsonify(time=time.time())
+
+
+@app.route('/photo')
+def photo_route():
+    print('photo')
+    img_io = io.BytesIO()
+    try:
+        script_dir = os.path.dirname(__file__)
+        rel_path = "../public/01.png"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        print("path", abs_file_path)
+        image = Image.open(abs_file_path)
+        image.save(img_io, 'PNG', quality=70)
+        img_io.seek(0)
+    except Exception as error:
+        print('error', error)
+        traceback.print_tb(error.__traceback__)
+    return send_file(img_io, mimetype='image/png')
+
+
+def catch_all():
+    print('catch')
+    return "catch"
