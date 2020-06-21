5c281795d85486e84b610c3da08fb5788d5eaf3a
Anna <admin@lensesrequired.com>
2020-06-21 17:40:37 -0500

    allow all cors

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ import base64
 import os
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"*": {"origins": "*"}})
+cors = CORS(app)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(app,
           version='0.1',
