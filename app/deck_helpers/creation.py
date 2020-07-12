3745a7040102b28e3c6ae9bf335acc2475a516fa
Anna <admin@lensesrequired.com>
2020-07-11 19:12:29 -0500

    Update pdf creation with documentation

--- app/deck_helpers/creation.py
+++ app/deck_helpers/creation.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from ..card_helpers import creation as card_creation
 
+# the pdf image's positions for the top left corner of a given card index
 positions = {
     0: (30, 45),
     1: (795, 45),
@@ -16,23 +17,48 @@ cards_on_page = 8
 
 
 def get_position(photo_index):
-    return positions[photo_index] if photo_index < cards_on_page else (0, 0)
+    """
+    Returns the corner position for a given index for an image
+    :param photo_index: int
+    :return: tuple (x,y)
+    """
+    return positions.get(photo_index, (0, 0))
 
 
 def create_pdf(cards):
+    """
+    Creates a page images for a list of cards
+    :param cards: array of cards
+    :return: array of images
+    """
     pages = []
     all_cards = []
+
     for c in cards:
+        # get card background and associated font color from card
+        card_art, font_color = card_creation.get_art(c['art'].split('/'))
+        # create card with art as background
+        card_creation.create_card(c, card_art, font_color)
+        # add the correct qty of the card to the list of all cards
         for i in range(int(c['qty'])):
-            all_cards.append(c)
-    for index, card in enumerate(all_cards):
-        photo_index = index % cards_on_page
-        if (photo_index == 0):
+            all_cards.append(card_art)
+
+    # put each card art on a page
+    for index, card_art in enumerate(all_cards):
+        # mod the index to by the max number of cards on a page to get what number card on a page it is
+        card_index = index % cards_on_page
+
+        # if it's the first card on a page, create a new page first
+        if (card_index == 0):
             pages.append(Image.new('RGB', (2312, 2992), (255, 255, 255)))
+
+        # get the correct page for the card
         page = pages[index // cards_on_page]
-        card_art, font_color = card_creation.get_art(card['art'].split('/'))
-        card_creation.create_card(card, card_art, font_color)
-        if (photo_index > 5):
+
+        # rotate card 6 and 7 side ways to maximize cards on a piece of paper (woohoo, saving the planet!)
+        if (card_index > 5):
             card_art = card_art.rotate(90, expand=1)
-        page.paste(card_art, get_position(photo_index))
+
+        # put card in specified spot onto the page
+        page.paste(card_art, get_position(card_index))
     return pages
